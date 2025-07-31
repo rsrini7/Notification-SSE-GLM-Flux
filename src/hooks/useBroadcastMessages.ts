5df01cf8d7975dc8196a490dcaa2d810a2558681
Srini <7rsrini@gmail.com>
2025-07-31 22:46:39 +0530

    feat(broadcast): handle MESSAGE_REMOVED event in useBroadcastMessages hook

--- src/hooks/useBroadcastMessages.ts
+++ src/hooks/useBroadcastMessages.ts
@@ -85,6 +85,9 @@ export const useBroadcastMessages = (options: UseBroadcastMessagesOptions) => {
       case 'HEARTBEAT':
         break;
       
+      case 'MESSAGE_REMOVED':
+        break;
+      
       default:
         console.log('Unhandled SSE event type:', event.type);
     }
