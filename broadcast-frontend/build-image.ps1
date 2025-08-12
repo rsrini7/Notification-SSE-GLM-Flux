39d32502363ce36b261116710518b3081e6e73f9
Srini <7rsrini@gmail.com>
2025-08-12 10:26:20 +0530

    chore: update nginx image version to 1.0.5 in deployment files

--- broadcast-frontend/build-image.ps1
+++ broadcast-frontend/build-image.ps1
@@ -1,4 +1,4 @@
 Write-Output "npm run build"
 npm run build
-Write-Output "docker build --no-cache -t broadcast-nginx:1.0.4 ."
-docker build --no-cache -t broadcast-nginx:1.0.4 .
+Write-Output "docker build --no-cache -t broadcast-nginx:1.0.5 ."
+docker build --no-cache -t broadcast-nginx:1.0.5 .
