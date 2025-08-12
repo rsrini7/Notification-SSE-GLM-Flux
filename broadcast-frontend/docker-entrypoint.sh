ca9e3d58fe657c88c9f22c0edbde59347ea3fb66
Srini <7rsrini@gmail.com>
2025-08-12 19:14:07 +0530

    refactor(nginx): simplify nginx configuration and entrypoint script
    - Replace hardcoded frontend location block with dynamic configuration based on environment
    - Clean up docker-compose.yml by removing commented code and organizing environment variables
    - Update entrypoint script to handle frontend routing differently in dev vs prod environments

--- broadcast-frontend/docker-entrypoint.sh
+++ broadcast-frontend/docker-entrypoint.sh
@@ -14,6 +14,24 @@ else
   KEY_FILE_NAME="tls.key"
 fi
 
+if [ "$NGINX_ENV" = "dev" ]; then
+  FRONTEND_CONFIG='
+    location / {
+        proxy_pass https://host.docker.internal:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        proxy_set_header Host $host;
+    }'
+else
+  FRONTEND_CONFIG='
+    location / {
+        root   /usr/share/nginx/html;
+        index  index.html index.htm;
+        try_files $uri $uri/ /index.html;
+    }'
+fi
+
 # UPDATED: Export and substitute both new backend URLs
 export ADMIN_BACKEND_URL
 export USER_BACKEND_URL
@@ -21,9 +39,13 @@ export CERT_FILE_NAME
 export KEY_FILE_NAME
 envsubst '$ADMIN_BACKEND_URL,$USER_BACKEND_URL,$CERT_FILE_NAME,$KEY_FILE_NAME' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf.tmp
 
-awk -v resolver_config="$RESOLVER_CONFIG" '{
+awk \
+  -v resolver_config="$RESOLVER_CONFIG" \
+  -v frontend_config="$FRONTEND_CONFIG" \
+'{
   sub("##RESOLVER_DIRECTIVE##", resolver_config);
-print
+  sub("##FRONTEND_LOCATION_BLOCK##", frontend_config);
+  print
 }' /etc/nginx/nginx.conf.tmp > /etc/nginx/nginx.conf
 
 rm /etc/nginx/nginx.conf.tmp
