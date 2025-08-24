668709dea36f4ae56d77f87326faf4859850d48c
Srini <7rsrini@gmail.com>
2025-08-24 10:27:19 +0530

    refactor: import Optional for better null handling

--- broadcast-microservice/broadcast-user-service/src/main/java/com/example/broadcast/user/service/KafkaOrchestratorConsumerService.java
+++ broadcast-microservice/broadcast-user-service/src/main/java/com/example/broadcast/user/service/KafkaOrchestratorConsumerService.java
@@ -23,6 +23,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
